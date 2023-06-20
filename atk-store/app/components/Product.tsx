118b051fbd9275c8c0e09d7736a959f480470d82
Adam Khodier <khodieradam1297@gmail.com>
2023-06-20 10:42:58 -0400

    dynamic product description page using id in the route

--- atk-store/app/components/Product.tsx
+++ atk-store/app/components/Product.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image"
 import priceFormat from "@/util/PriceFormat"
 import { ProductType } from "@/types/ProductType"
+import Link from "next/link"
 
-export default function Product({name, image, price}: ProductType){
+export default function Product({name, image, price, id}: ProductType){
     return(
-        <div className="text-gray-700">
-            <h1 className="font-medium">{name}</h1>
-            <Image className="w-full object-cover" 
-            src={image} alt={name} 
-            width={400} height={400}/>
-            <h3 className="text-teal-500 font-medium py-2">{price !== null ? priceFormat(price) : 'N/A'}</h3>
-        </div>
+        <Link href={`${/product/id}`}>
+            <div className="text-gray-700">
+                <h1 className="font-medium">{name}</h1>
+                <Image className="w-full object-cover" 
+                src={image} alt={name} 
+                width={400} height={400}/>
+                <h3 className="text-teal-500 font-medium py-2">{price !== null ? priceFormat(price) : 'N/A'}</h3>
+            </div>
+        </Link>
     )
 }
