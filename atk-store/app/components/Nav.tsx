0b218af83e023817c90ac1baf5836a1b85a682f2
Adam Khodier <khodieradam1297@gmail.com>
2023-06-21 15:33:21 -0400

    fixed desc spacing and linked home

--- atk-store/app/components/Nav.tsx
+++ atk-store/app/components/Nav.tsx
@@ -3,11 +3,14 @@
 import { Session } from 'next-auth'
 import { signIn } from 'next-auth/react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Nav({ user }: Session){
     return(
-        <nav className='flex justify-between items-center py-8'>
-            <h1>Nav Styled</h1>
+        <nav className='flex justify-between items-center p-8'>
+            <Link href={"/"}>
+            <h1>ATK-STORE</h1>
+            </Link>
             <ul className='flex items-center gap-12'>
                 {!user && (
                     <li className='bg-teal-600 text-white py-2 px-4 rounded-md'>
@@ -24,7 +27,7 @@ export default function Nav({ user }: Session){
                             width={48} height={48}/>
                         </li>
                         <li>
-                            Dashboard
+                            Hamburger Dashboard
                         </li>
                     </div>
                 )}
--- atk-store/app/product/[id]/page.tsx
+++ atk-store/app/product/[id]/page.tsx
@@ -5,19 +5,16 @@ import priceFormat from "@/util/PriceFormat"
 export default async function Product({searchParams}: SearchParamType) {
     return(
     <div className="flex-justify-between gap-24 p-12 text-gray-700">
-        <Image
-        src={searchParams.image}
-        alt={searchParams.name}
-        width={600}
-        height={600}
-        />
-        <div>
-            <h1>{searchParams.name}</h1>
-            <p>{searchParams.description}</p>
-        </div>
-        <div className="flex gap-2">
-            <p className="font-bold text-teal-500">{searchParams.unit_amount !== null ? priceFormat(searchParams.unit_amount) : "N/A"}</p>
-        </div>
+        <h1 className="text-2xl py-2 font-medium">{searchParams.name}</h1>
+            <Image 
+            src={searchParams.image}
+            alt={searchParams.name}
+            width={600}
+            height={600}
+            />
+        <p className="py-2">{searchParams.description}</p>   
+        <p className="font-bold text-teal-500">{searchParams.unit_amount !== null ? priceFormat(searchParams.unit_amount) : "N/A"}</p>
+        <button className="my-12 text-white py-2 px-6 font-medium rounded-md bg-teal-500">Add to Cart</button>
     </div>
     )
 }
