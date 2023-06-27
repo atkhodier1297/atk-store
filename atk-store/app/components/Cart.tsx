a3253a37b716597e223c5ae3e568204e137ac7c5
Adam Khodier <khodieradam1297@gmail.com>
2023-06-26 20:46:30 -0400

    worked on cart ui and cond rendered checkout btn based on cart array length

--- atk-store/app/components/Cart.tsx
+++ atk-store/app/components/Cart.tsx
@@ -1,15 +1,41 @@
 "use client";
 import Image from "next/image";
 import { useCartStore } from "@/store";
+import priceFormat from "@/util/PriceFormat";
 
 export default function Cart() {
   const cartStore = useCartStore();
   console.log(cartStore.isOpen);
   return (
-    <div onClick={() => cartStore.toggleCart() } className="fixed w-full h-screen left-0 top-0 bg-black/25">
-      <div onClick={(e) => e.stopPropagation() } className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray">
+    <div
+      onClick={() => cartStore.toggleCart()}
+      className="fixed w-full h-screen left-0 top-0 bg-black/25"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray"
+      >
         <h1>Here's your shopping list! 📃</h1>
+        {cartStore.cart.map((item) => (
+          <div className="flex py-4 gap-4">
+            <Image
+              className="rounded-md h-24"
+              src={item.image}
+              alt={item.name}
+              width={120}
+              height={120}
+            />
+            <div>
+              <h2>{item.name}</h2>
+              <h2>Quantity: {item.quantity}</h2>
+              <p className="text-sm">{item.unit_amount && priceFormat(item.unit_amount)}</p>
+            </div>
+          </div>
+        ))}
+        { cartStore.cart.length > 0 ?
+        <button className="mt-4 w-full text-white py-2 px-6 font-medium rounded-md bg-teal-500">Checkout</button>
+        : null }
       </div>
     </div>
-  )
+  );
 }
