f426fc1b6d7cbfb7467aed75d3b5d7a3f5785fff
Adam Khodier <khodieradam1297@gmail.com>
2023-06-28 22:21:19 -0400

    added goog font using next library and rendered out cart totals conditonally

--- atk-store/app/components/Cart.tsx
+++ atk-store/app/components/Cart.tsx
@@ -6,18 +6,25 @@ import { IoAddCircle, IoRemoveCircle } from "react-icons/io5";
 
 export default function Cart() {
   const cartStore = useCartStore();
-  console.log(cartStore.isOpen);
+
+  const totalPrice = cartStore.cart.reduce((acc, item) => {
+    return acc + item.unit_amount! * item.quantity!;
+  }, 0);
+
   return (
     <div
       onClick={() => cartStore.toggleCart()}
-      className="fixed w-full h-screen left-0 top-0 bg-black/25"
+      className="fixed w-full h-screen left-0 top-0 bg-black/50"
     >
       <div
         onClick={(e) => e.stopPropagation()}
         className="bg-white absolute right-0 top-0 w-1/2 h-screen p-12 overflow-x-scroll text-gray"
       >
-        { cartStore.cart.length < 1 ? <h1>Your shopping cart is empty...😔</h1> :
-        <h1>Here's your shopping list! 📃</h1> }
+        {cartStore.cart.length < 1 ? (
+          <h1>Your shopping cart is empty...😔</h1>
+        ) : (
+          <h1>Here's your shopping list! 📃</h1>
+        )}
         {cartStore.cart.map((item) => (
           <div className="flex py-4 gap-4">
             <Image
@@ -31,7 +38,8 @@ export default function Cart() {
               <h2>{item.name}</h2>
               <div className="flex gap-2">
                 <h2>Quantity: {item.quantity}</h2>
-                <button onClick={() =>
+                <button
+                  onClick={() =>
                     cartStore.removeProduct({
                       id: item.id,
                       image: item.image,
@@ -64,9 +72,12 @@ export default function Cart() {
           </div>
         ))}
         {cartStore.cart.length > 0 ? (
-          <button className="mt-4 w-full text-white py-2 px-6 font-medium rounded-md bg-teal-500">
-            Checkout
-          </button>
+          <div>
+            <p>Cart Total: {priceFormat(totalPrice)}</p>
+            <button className="mt-4 w-full text-white py-2 px-6 font-medium rounded-md bg-teal-500">
+              Checkout
+            </button>
+          </div>
         ) : null}
       </div>
     </div>
