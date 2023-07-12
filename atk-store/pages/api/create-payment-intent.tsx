7870ed822e3846d1002fda5b787a426fcd1181ad
Adam Khodier <khodieradam1297@gmail.com>
2023-07-11 22:17:56 -0400

    prisma order data setup create the model tommorrow

--- atk-store/pages/api/create-payment-intent.tsx
+++ atk-store/pages/api/create-payment-intent.tsx
@@ -2,10 +2,18 @@ import Stripe from "stripe";
 import {NextApiRequest, NextApiResponse } from "next"
 import { authOptions } from "./auth/[...nextauth]";
 import { getServerSession } from "next-auth"
+import { AddCartType } from "@/types/AddCartType";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
     apiVersion: "2022-11-15",
 })
+
+const calculateOrderAmount = (items: AddCartType[]) => {
+    const totalPrice = items.reduce((acc, item) => {
+        return acc + item.unit_amount! * item.quantity!
+    }, 0)
+    return totalPrice
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
     //Get user
@@ -16,7 +24,25 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     //extract data from the body
     const {items, payment_intent_id } = req.body
 
-    res.status(200).json({ userSession })
-    return
     //data for the order
+    const orderData = {
+        user: { connect: { id: userSession.user?.id } },
+        amount: calculateOrderAmount(items),
+        currency: 'usd',
+        status: 'pending',
+        paymentIntentID: payment_intent_id,
+        products: {
+            create: items.map((item) => ({
+                name: item.name,
+                description: item.description,
+                unit_amount: item.unit_amount,
+                quantity: item.quantity,
+            }))
+        }
+    }
+    
+    res.status(200).json({ userSession })
+
+    return
+    
 }
