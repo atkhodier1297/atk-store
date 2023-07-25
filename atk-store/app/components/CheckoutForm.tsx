f06ab928de59a99bf6781505ae6bdc8b4fd26f81
Adam Khodier <khodieradam1297@gmail.com>
2023-07-25 14:41:30 -0400

    initilized stripe payment element and handled errors if stripe or stripe elements are not working

--- atk-store/app/components/CheckoutForm.tsx
+++ atk-store/app/components/CheckoutForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { PaymentElement, useStripe, useElements } from "@stripe/react-stripe-js"
 import priceFormat from "@/util/PriceFormat"
 import { useCartStore } from "@/store"
@@ -16,11 +16,42 @@ export default function CheckoutForm({clientSecret}: {clientSecret: string}){
     }, 0)
     const formattedPrice = priceFormat(totalPrice)
 
+    useEffect(() => {
+        if(!stripe){
+            return
+        }
+        if(!clientSecret){
+            return
+        }
+    },[stripe])
+
+    const handleSubmit = async (e:React.FormEvent) => {
+        e.preventDefault()
+        if(!stripe || !elements) {
+            return
+        }
+        setIsLoading(true)
+        stripe.confirmPayment({
+            elements,
+            redirect: 'if_required'
+        })
+        .then((result) => {
+            if(!result.error){
+                cartStore.setCheckout("success")
+            }
+            setIsLoading(false)
+        })
+    }
+
     return(
-        <form id='payment-form'>
+        <form onSubmit={handleSubmit} id='payment-form'>
             <PaymentElement id="payment-element" options={{layout: 'tabs'}} />
             <h1>Total: {formattedPrice}</h1>
-            <button id="submit" disabled={isLoading || !stripe || !elements}></button>
+            <button id="submit" disabled={isLoading || !stripe || !elements}>
+                <span id="button-text">
+                    {isLoading ? <span>Processing 👀</span> : <span>Pay Now ✅</span>}
+                </span>
+            </button>
         </form>
     )
 }
