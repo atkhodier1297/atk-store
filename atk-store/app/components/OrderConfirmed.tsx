3a5f3197753333926c31109b662ec6796211c16b
Adam Khodier <khodieradam1297@gmail.com>
2023-07-26 11:11:26 -0400

    order conf closes the cart comp and now routes to emoty dash while clearing cart state and setting payment intend to empty string

--- atk-store/app/components/OrderConfirmed.tsx
+++ atk-store/app/components/OrderConfirmed.tsx
@@ -1,40 +1,44 @@
-'use client'
-import { motion } from "framer-motion"
-import Image from "next/image"
-import dancing from '@/public/dancing.gif'
-import Link from "next/link"
-import { useCartStore } from "@/store"
+"use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import dancing from "@/public/dancing.gif";
+import Link from "next/link";
+import { useCartStore } from "@/store";
+import { useEffect } from "react";
 
-export default function OrderConfirmed(){
+export default function OrderConfirmed() {
+  const cartStore = useCartStore();
 
-    const cartStore = useCartStore()
+  useEffect(() => {
+    cartStore.setPaymentIntent("");
+    cartStore.clearCart();
+  }, []);
 
-    return(
-        <motion.div
-        className="flex items-center justify-center my-12"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        >
-            <div className="p-12 rounded-md text-center">
-                <h1 className="text-xl font-medium">Your order has been placed. 🚀</h1>
-                <h2 className="text-sm my-4">Check your email for your receipt.</h2>
-                <Image src={dancing} className="py-8" alt="dancing monkey"/>
-                <div>
-                <Link href={"/dashboard"}>
-                    <button className="font-medium">
-                        Check your Order
-                    </button>
-                </Link>
-                <button onClick={() => {
-                    cartStore.setCheckout("cart")
-                    cartStore.toggleCart()
-                }} className="font-medium"
-                >
-                    Create a new Order
-                </button>
-            </div>
-            </div>
-        
-        </motion.div>
-    )
+  const checkoutOrder = () => {
+    setTimeout(() => {
+      cartStore.setCheckout("cart");
+    }, 1000);
+    cartStore.toggleCart();
+  };
+
+  return (
+    <motion.div
+      className="flex items-center justify-center my-12"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <div className="p-12 rounded-md text-center">
+        <h1 className="text-xl font-medium">Your order has been placed. 🚀</h1>
+        <h2 className="text-sm my-4">Check your email for your receipt.</h2>
+        <Image src={dancing} className="py-8" alt="dancing monkey" />
+        <div className="flex items-center justify-center gap-12">
+          <Link href={"/dashboard"}>
+            <button onClick={checkoutOrder} className="font-medium">
+              Check your Order
+            </button>
+          </Link>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
---
+++ atk-store/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard(){
+    return(
+        <div>
+            <h1>Dashbooard</h1>
+        </div>
+    )
+}
