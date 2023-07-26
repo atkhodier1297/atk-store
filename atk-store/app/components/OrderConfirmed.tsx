fdea92439091d8786dadaf94b7f946aa4e9973f9
Adam Khodier <khodieradam1297@gmail.com>
2023-07-26 09:03:20 -0400

    basic styling for order confirmed page and buttons to clear state new order and to check your order

--- atk-store/app/components/OrderConfirmed.tsx
+++ atk-store/app/components/OrderConfirmed.tsx
@@ -2,17 +2,37 @@
 import { motion } from "framer-motion"
 import Image from "next/image"
 import dancing from '@/public/dancing.gif'
+import Link from "next/link"
+import { useCartStore } from "@/store"
 
 export default function OrderConfirmed(){
+
+    const cartStore = useCartStore()
+
     return(
         <motion.div
+        className="flex items-center justify-center my-12"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         >
-            <div>
-                <h1>Your order has been placed! 🚀</h1>
-                <h2>Check your email for your receipt</h2>
+            <div className="p-12 rounded-md text-center">
+                <h1 className="text-xl font-medium">Your order has been placed. 🚀</h1>
+                <h2 className="text-sm my-4">Check your email for your receipt.</h2>
                 <Image src={dancing} className="py-8" alt="dancing monkey"/>
+                <div>
+                <Link href={"/dashboard"}>
+                    <button className="font-medium">
+                        Check your Order
+                    </button>
+                </Link>
+                <button onClick={() => {
+                    cartStore.setCheckout("cart")
+                    cartStore.toggleCart()
+                }} className="font-medium"
+                >
+                    Create a new Order
+                </button>
+            </div>
             </div>
         
         </motion.div>
