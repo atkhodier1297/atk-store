b399e5890cfb786cca4d5899b9ea3baa43575391
Adam Khodier <khodieradam1297@gmail.com>
2023-08-02 21:55:28 -0400

    changed order route and decided to fetch orders in dashboard/pages

--- atk-store/app/dashboard/page.tsx
+++ atk-store/app/dashboard/page.tsx
@@ -1,7 +1,30 @@
-export default function Dashboard(){
-    return(
-        <div>
-            <h1>Dashbooard</h1>
-        </div>
-    )
+import { Prisma, PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
+
+const fetchOrders = async () => {
+  const prisma = new PrismaClient();
+  const user = await getServerSession(authOptions);
+  if (!user) {
+    return {message: "Not logged in"}
+  }
+  const orders = await prisma.order.findMany({
+    where: {
+      userId: user?.user?.id,
+    },
+    include: {
+      products: true,
+    },
+  });
+  return orders
+};
+
+export default async function Dashboard() {
+    const orders = await fetchOrders()
+    console.log(orders)
+  return (
+    <div>
+      <h1>Dashbooard</h1>
+    </div>
+  );
 }
