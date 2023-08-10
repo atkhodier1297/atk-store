541d6e86d0aa501c0c270e5ced2046d7dc6c0439
Adam Khodier <khodieradam1297@gmail.com>
2023-08-09 20:53:26 -0400

    added lottie animation for when client secret is being grabbed and stripe elements are still loading

--- atk-store/app/components/Checkout.tsx
+++ atk-store/app/components/Checkout.tsx
@@ -5,6 +5,7 @@ import { useCartStore } from "@/store";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import CheckoutForm from "./CheckoutForm";
+import OrderAnimation from "./OrderAnimation";
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
@@ -45,8 +46,10 @@ export default function Checkout() {
     },
   };
 
+  //renders orderani component while client secret is obtained and stripe is loading
   return (
     <div>
+      {!clientSecret && <OrderAnimation/>}
       {clientSecret && (
         <div>
           <Elements options={options} stripe={stripePromise}>
