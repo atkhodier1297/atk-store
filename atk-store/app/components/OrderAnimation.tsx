e392d0b4f42854690633e9398893bcc6e8a43883
Adam Khodier <khodieradam1297@gmail.com>
2023-08-09 21:04:42 -0400

    fixed animation size issue

--- atk-store/app/components/OrderAnimation.tsx
+++ atk-store/app/components/OrderAnimation.tsx
@@ -4,7 +4,7 @@ import order from "@/public/order.json";
 
 export default function OrderAnimation() {
   return (
-    <div className="flex items0center justify-center flex-col mt-24">
+    <div className="flex items-center justify-center flex-col">
       <motion.h1
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
