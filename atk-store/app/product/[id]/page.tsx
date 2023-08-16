c4d143a0f883affcdca54ad83a22315171d4bbf9
Adam Khodier <khodieradam1297@gmail.com>
2023-08-16 16:39:43 -0400

    debugging add to cart component

--- atk-store/app/product/[id]/page.tsx
+++ atk-store/app/product/[id]/page.tsx
@@ -22,9 +22,10 @@ export default async function Product({ searchParams }: SearchParamType) {
           ? priceFormat(searchParams.unit_amount)
           : "N/A"}
       </p>
-      <AddCart {...searchParams}
+      <AddCart
       id={searchParams.id}
       name={searchParams.name}
+      image={searchParams.image}
       unit_amount={searchParams.unit_amount}
       />
     </div>
